e6aa192f337bb8206f5eab9210e163121ae4f315
Fernando Vega <juanfe.vega@gmail.com>
2018-05-07 09:39:40 -0500

    Ajax con vue resources

---
+++ esencial/ajax-vue-resources/main.js
@@ -0,0 +1,23 @@
+const vm = new Vue({
+  el: 'main',
+  mounted(){
+    this.cargarPersonas();
+  },
+  data: {
+    personas: []
+  },
+
+  methods:{
+    cargarPersonas(){
+      this.$http.get('https://randomuser.me/api/?results=500')
+      .then((respuesta) => {
+        this.personas = respuesta.body.results;
+      });
+    }
+  },
+
+  computed:{
+
+  },
+
+});
