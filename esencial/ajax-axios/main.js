015d79ca366f718e79ad0e61b196fce3854ec635
Fernando Vega <juanfe.vega@gmail.com>
2018-05-07 09:51:33 -0500

    Ajax con axios

---
+++ esencial/ajax-axios/main.js
@@ -0,0 +1,26 @@
+const vm = new Vue({
+  el: 'main',
+  mounted(){
+    this.cargarPersonas();
+  },
+  data: {
+    personas: []
+  },
+
+  methods:{
+    cargarPersonas(){
+      /* this.$http.get('https://randomuser.me/api/?results=500')
+      .then((respuesta) => {
+        this.personas = respuesta.body.results;
+      }); */
+      axios.get('https://randomuser.me/api/?results=50').then((respuesta) => {
+        this.personas = respuesta.data.results;
+      })
+    }
+  },
+
+  computed:{
+
+  },
+
+});
