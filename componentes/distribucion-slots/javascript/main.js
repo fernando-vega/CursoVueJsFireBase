539ede1b1cde1afb19e21f92e00e3ac4a553684e
Fernando Vega <juanfe.vega@gmail.com>
2018-05-08 17:35:23 -0500

    Distribución de contenido con slots

---
+++ componentes/distribucion-slots/javascript/main.js
@@ -0,0 +1,26 @@
+Vue.component('alerta', {
+  props: ['tipo','posicion'],
+  template: `
+            <section class="alerta" :class="[tipo,posicion]">
+              <header class="alerta__header">
+                <slot name="header">Hola</slot> 
+              </header>
+              <div class="alerta__contenido">
+                <slot name="content">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, odio facere. Quam quisquam aspernatur amet, quae voluptas repellendus voluptatem cum ut facilis, assumenda provident odio facere rerum, similique expedita ratione.
+                </slot>
+              </div>
+              <footer class="alerta__footer">
+                <slot name="footer">Este es el footer</slot>
+              </footer>
+            </section> `
+});
+
+new Vue({
+
+  el: 'main',
+  data: {
+    prueba: 'Hola'
+  },
+
+});
